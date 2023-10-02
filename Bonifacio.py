e5c85b7daddf4177805301058f21f0bfeb6ed98b
junnie <mjbonifacio@tip.edu.ph>
2023-10-02 12:21:20 +0800

    SOurce code updated

--- Bonifacio.py
+++ Bonifacio.py
@@ -3,11 +3,4 @@ a = input("What is your first name: ")
 b = input("What is your middle name: ")
 c = input("What is your last name: ")
 
-print ( a," "+ b," " + ," "c )
-=======
-a = input("What is your first name: ")
-b = input("What is your middle name: ")
-c = input("What is your last name: ")
-
 print ( a," "+ b," " + c )
->>>>>>> 6eb46eed5c593dbe4577d15876c2455949c595ca
